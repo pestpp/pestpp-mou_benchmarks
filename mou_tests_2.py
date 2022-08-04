67acc73318119c98308aa6b04c24bba8f2f3f9f7
jdub <JWhite@localadmins-MacBook-Pro.local>
2022-08-04 09:37:23 -0500

    testing for tied pars and dp-op consistency

--- mou_tests_2.py
+++ mou_tests_2.py
@@ -1719,9 +1719,98 @@ def simplex_invest_1():
 
 
 
+def zdt1_tied_test():
+    t_d = mou_suite_helper.setup_problem("zdt1",False,False)
+    #df = pd.read_csv(os.path.join(t_d,"prior.csv"),index_col=0)
+    #df.loc[:,"_risk_"] = 0.95
+    #print(df.columns)
+    #df.to_csv(os.path.join(t_d,"prior.csv"))
+    pst = pyemu.Pst(os.path.join(t_d,"zdt1.pst"))
+    par = pst.parameter_data
+    first = pst.par_names[1]
+    others = pst.par_names[2:-2]
+    par.loc[others,"partrans"] = "tied"
+    par.loc[others,"partied"] = first
+    par.loc[others,"pargp"] = "tiedup"
+    #par.loc[first,"parval1"] = 1.0
+
+    #pst.pestpp_options["mou_dv_population_file"] = "prior.csv"
+    #pst.pestpp_options["opt_chance_points"] = "single"
+    pst.pestpp_options["opt_recalc_chance_every"] = 1000
+    pst.pestpp_options["opt_dec_var_groups"] = "decvars"
+    pst.pestpp_options["mou_save_population_every"] = 1
+    #pst.pestpp_options["opt_stack_size"] = 10
+    #pst.pestpp_options["opt_par_stack"] = "prior.csv"
+    pst.pestpp_options["mou_generator"] = "de"
+    pst.pestpp_options["mou_population_size"] = 10
+    pst.control_data.noptmax = 0
+    pst.write(os.path.join(t_d,"zdt1.pst"))
+    pyemu.os_utils.run("{0} {1}".format(exe_path,"zdt1.pst"),cwd=t_d)
+    df = pd.read_csv(os.path.join(t_d,"dv.dat"),header=None,names=["dv","val"],delim_whitespace=True)
+    df.index = df.dv
+    assert (df.loc[others,"val"] - df.loc[first,"val"]).apply(np.abs).sum() == 0.0
+
+    m1 = os.path.join("mou_tests","zdt1_tied_test")
+    pst.control_data.noptmax = 3
+    pst.write(os.path.join(t_d,"zdt1.pst"))
+    pyemu.os_utils.start_workers(t_d,exe_path,"zdt1.pst",10,worker_root="mou_tests",
+                                 master_dir=m1,verbose=True,port=port)
+    for i in range(pst.control_data.noptmax+1):
+        dp = pd.read_csv(os.path.join(m1,"zdt1.{0}.dv_pop.csv").format(i),index_col=0)
+        op = pd.read_csv(os.path.join(m1, "zdt1.{0}.obs_pop.csv").format(i), index_col=0)
+        assert dp.shape[0] == op.shape[0]
+        print(dp.shape[0])
+        for ii in range(dp.shape[0]):
+            assert dp.index[ii] == op.index[ii]
+            ii = dp.index[ii]
+            d = np.abs(dp.loc[ii,others].values - dp.loc[ii,first]).sum()
+            assert d == 0,d
+
+        dp = pd.read_csv(os.path.join(m1, "zdt1.{0}.archive.dv_pop.csv").format(i), index_col=0)
+        op = pd.read_csv(os.path.join(m1, "zdt1.{0}.archive.obs_pop.csv").format(i), index_col=0)
+        assert dp.shape[0] == op.shape[0]
+        print(dp.shape[0])
+        for ii in range(dp.shape[0]):
+            assert dp.index[ii] == op.index[ii]
+            ii = dp.index[ii]
+            d = np.abs(dp.loc[ii, others].values - dp.loc[ii, first]).sum()
+            assert d == 0, d
+
+    pst.pestpp_options["save_binary"] = True
+    pst.write(os.path.join(t_d, "zdt1.pst"))
+    m1 = os.path.join("mou_tests", "zdt1_tied_test")
+    pyemu.os_utils.start_workers(t_d, exe_path, "zdt1.pst", 10, worker_root="mou_tests",
+                                 master_dir=m1, verbose=True, port=port)
+    for i in range(pst.control_data.noptmax + 1):
+        dp = pyemu.ParameterEnsemble.from_binary(pst=pst, filename=os.path.join(m1, "zdt1.{0}.dv_pop.jcb").format(i))
+        op = pyemu.ObservationEnsemble.from_binary(pst=pst, filename=os.path.join(m1, "zdt1.{0}.obs_pop.jcb").format(i))
+        assert dp.shape[0] == op.shape[0]
+        print(dp.shape[0])
+        for ii in range(dp.shape[0]):
+            assert dp.index[ii] == op.index[ii]
+            ii = dp.index[ii]
+            d = np.abs(dp._df.loc[ii, others].values - dp._df.loc[ii, first]).sum()
+            assert d == 0, d
+
+        dp = pyemu.ParameterEnsemble.from_binary(pst=pst,
+                                                 filename=os.path.join(m1, "zdt1.{0}.archive.dv_pop.jcb").format(i))
+        op = pyemu.ObservationEnsemble.from_binary(pst=pst,
+                                                   filename=os.path.join(m1, "zdt1.{0}.archive.obs_pop.jcb").format(i))
+        assert dp.shape[0] == op.shape[0]
+        print(dp.shape[0])
+        for ii in range(dp.shape[0]):
+            assert dp.index[ii] == op.index[ii]
+            ii = dp.index[ii]
+            d = np.abs(dp._df.loc[ii, others].values - dp._df.loc[ii, first]).sum()
+            #print(dp._df.loc[ii,others].values)
+            #print(dp._df.loc[ii, first])
+            assert d == 0, d
+
+
 if __name__ == "__main__":
         
-    shutil.copy2(os.path.join("..","exe","windows","x64","Debug","pestpp-mou.exe"),os.path.join("..","bin","pestpp-mou.exe"))
+    #shutil.copy2(os.path.join("..","exe","windows","x64","Debug","pestpp-mou.exe"),os.path.join("..","bin","pestpp-mou.exe"))
+    zdt1_tied_test()
     #basic_pso_test()
 
     #shutil.copy2(os.path.join("..", "bin", "win", "pestpp-mou.exe"),
@@ -1729,7 +1818,7 @@ if __name__ == "__main__":
     #basic_pso_test()
     #risk_obj_test()
     #invest_2()
-    chance_consistency_test()
+    #chance_consistency_test()
     #invest_3()
     # mou_suite_helper.start_workers("zdt1")
     #all_infeas_test()
